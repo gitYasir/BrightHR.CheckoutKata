50b83406cb0693fc1dbc5250dfa2e4cf9bc8c34b
Yasir <yasir.ibrahim@hotmail.co.uk>
2024-03-18 18:16:20 +0000

    added pricing rules

--- CheckoutApp/Ckeckout.cs
+++ CheckoutApp/Ckeckout.cs
@@ -10,4 +10,16 @@ namespace CheckoutApp {
         int GetTotalPrice();
     }
 
+    public class ItemPricing {
+        public char SKU { get; set; }
+        public int UnitPrice { get; set; }
+        public (int Quantity, int SpecialPrice) SpecialPrice { get; set; }
+
+        public ItemPricing( char sku, int unitPrice, int specialQuantity = 0, int specialPrice = 0 ) {
+            SKU = sku;
+            UnitPrice = unitPrice;
+            SpecialPrice = (specialQuantity, specialPrice);
+        }
+    }
+
 }
