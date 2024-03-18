5e80fca66d90a26a4d6fe794754b2a4ba348d3c1
Yasir <yasir.ibrahim@hotmail.co.uk>
2024-03-18 21:35:32 +0000

    all tests passing

--- Checkout.Tests/CheckoutTests.cs
+++ Checkout.Tests/CheckoutTests.cs
@@ -47,6 +47,68 @@ namespace CheckoutTests {
 
                 Assert.That( checkout.scannedItems[ item[ 0 ] ], Is.EqualTo( 2 ) );
             }
+            [Test]
+            public void GetTotalPrice_Should_Return_Total_Price_Of_Items() {
+                var pricingRules = new List<ItemPricing>
+                {
+                new ItemPricing('A', 50),
+                new ItemPricing('B', 30),
+                new ItemPricing('C', 20),
+                new ItemPricing('D', 15)
+            };
+                var checkout = new Checkout( pricingRules );
+
+                checkout.Scan( "A" );
+                checkout.Scan( "B" );
+                checkout.Scan( "C" );
+                checkout.Scan( "D" );
+
+                int expectedTotalPrice = 50 + 30 + 20 + 15;
+
+                Assert.That( checkout.GetTotalPrice(), Is.EqualTo( expectedTotalPrice ) );
+            }
+            [Test]
+            public void GetTotalPrice_Should_Return_Correct_Price_With_Special_Offer() {
+                // Arrange
+                var pricingRules = new List<ItemPricing>
+                {
+        new ItemPricing('A', 50, 3, 130),
+        new ItemPricing('B', 30, 2, 45),
+        new ItemPricing('C', 20),
+        new ItemPricing('D', 15)
+    };
+                var checkout = new Checkout( pricingRules );
+
+                // Act
+                checkout.Scan( "A" );
+                checkout.Scan( "A" );
+                checkout.Scan( "A" );
+
+                // Assert
+                Assert.AreEqual( 130, checkout.GetTotalPrice() );
+            }
+
+            [Test]
+            public void GetTotalPrice_Should_Return_Correct_Price_With_Default_Price() {
+                // Arrange
+                var pricingRules = new List<ItemPricing>
+                {
+        new ItemPricing('A', 50),
+        new ItemPricing('B', 30),
+        new ItemPricing('C', 20),
+        new ItemPricing('D', 15)
+    };
+                var checkout = new Checkout( pricingRules );
+
+                // Act
+                checkout.Scan( "A" );
+                checkout.Scan( "B" );
+                checkout.Scan( "C" );
+                checkout.Scan( "D" );
+
+                // Assert
+                Assert.AreEqual( 50 + 30 + 20 + 15, checkout.GetTotalPrice() );
+            }
 
         }
     }
